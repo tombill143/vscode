49bc9aa0e8b4eb6983bfe30f94412161cb2e9c97
Tom Billington <tombill143@gmail.com>
2022-08-23 13:03:32 +0200

    commit

--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -3,12 +3,16 @@
 //document.querySelector('div').forEach(div) => {
 //}
 
-
 //come back to this aswell
 //document.querySelectorAll('div').forEach(div) => {
 //    div.style.transform = `translate(${Math.random() * 1000}px, ${Math.random() * 1000}px´)
 //}
 
-document.querySelectorAll('div').forEach(div => {
-        div.style.transform = `translate(${Math.random() * 1000}px, ${Math.random() * 1000}px´)
-    }
+setInterval(() => {
+  document.querySelectorAll("div").forEach((div) => {
+    div.style.transform = `translate(${Math.random() * 100}vw, ${
+      Math.random() * 100
+    }vh)`;
+    div.style.backgroundColor = `hsl(${Math.random() * 360}, 70%, 70%)`;
+  });
+}, 5000);

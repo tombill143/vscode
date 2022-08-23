eb91a2885a076cc566567af69b0e0427ab639b8a
Tom Billington <tombill143@gmail.com>
2022-08-23 12:59:48 +0200

    can i commit?

---
+++ bubbles/bubbles.js
@@ -0,0 +1,14 @@
+//Please come back to this one
+
+//document.querySelector('div').forEach(div) => {
+//}
+
+
+//come back to this aswell
+//document.querySelectorAll('div').forEach(div) => {
+//    div.style.transform = `translate(${Math.random() * 1000}px, ${Math.random() * 1000}px´)
+//}
+
+document.querySelectorAll('div').forEach(div => {
+        div.style.transform = `translate(${Math.random() * 1000}px, ${Math.random() * 1000}px´)
+    }

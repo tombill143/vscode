928cdc835ffd4783f6cbcf2d6d04237cc050489b
Tom Billington <tombill143@gmail.com>
2022-08-23 10:28:30 +0200

    Javascript file made

--- git/git.js
+++ git/git.js
@@ -1,1 +1,10 @@
+let tom = {
+  name: "Tom Billington",
+  jobs: ["bum"],
+  previousJobs: ["CEO of Awesome inc"],
+  status: "student who would love to create a huge house of cards",
+  numberOfSheep: 13,
+  question: "why do I need to tell you how many sheep there are in my life?",
+};
+
 console.log("here we go");
